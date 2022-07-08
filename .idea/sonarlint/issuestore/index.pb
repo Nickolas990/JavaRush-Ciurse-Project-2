4025d47732098292944eb6008e2f57cf118ca5a3
hrom_90@hotmail.com <71078401+Nickolas990@users.noreply.github.com>
2022-07-08 09:09:16 +0300

    added logger log4j

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+Š
+Zsrc/main/java/com/ru/javarush/echo/nikolaymelnikov/javarushproject/creatures/Creature.java,8\5\85fb130ee7eeb8d7df9c1534ece22187fc89cb15
+›
+ksrc/main/java/com/ru/javarush/echo/nikolaymelnikov/javarushproject/services/rannables/ActingOfTheWorld.java,6\4\644e9de0c1efb9c1d0a5abd1f342c30fab6ec81c
+‡
+Wsrc/main/java/com/ru/javarush/echo/nikolaymelnikov/javarushproject/services/Runner.java,4\4\44a253f6b5a85ea088a8395ae01b28f98676fb78
+Š
+Zsrc/main/java/com/ru/javarush/echo/nikolaymelnikov/javarushproject/settings/LuckTable.java,3\9\39ded4dd5615637a5d063ae4c25f962bd3755142
+‰
+Ysrc/main/java/com/ru/javarush/echo/nikolaymelnikov/javarushproject/settings/Settings.java,6\b\6bfc32fbbfe2600e0453906b81dea856a6deb1be
+J
+dependency-reduced-pom.xml,8\a\8a88dfd5af6f2db5a5431581b56e901f5f143d92
--- src/main/java/com/ru/javarush/echo/nikolaymelnikov/javarushproject/util/StatisticsPrinter.java
+++ src/main/java/com/ru/javarush/echo/nikolaymelnikov/javarushproject/util/StatisticsPrinter.java
@@ -5,26 +5,33 @@ import com.ru.javarush.echo.nikolaymelnikov.javarushproject.island.Cell;
 import com.ru.javarush.echo.nikolaymelnikov.javarushproject.island.Island;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
+
+
 @Getter
 @Setter
 public class StatisticsPrinter implements Printer {
 
-    public Map<String, Long> quantityOfAnimals = new ConcurrentHashMap<>();
-    public Map<String, Long> quantityOfPlants = new ConcurrentHashMap<>();
-    public AtomicInteger carnivoresQuantity = new AtomicInteger(0);
-    public AtomicInteger herbivoresQuantity = new AtomicInteger(0);
+    private static final Logger loggerToConsole = Logger.getLogger("APP1");
+    private static final Logger loggerToFile = Logger.getLogger("APP2");
+    private Map<String, Long> quantityOfAnimals = new ConcurrentHashMap<>();
+    private Map<String, Long> quantityOfPlants = new ConcurrentHashMap<>();
+    private AtomicInteger carnivoresQuantity = new AtomicInteger(0);
+    private AtomicInteger herbivoresQuantity = new AtomicInteger(0);
     private ExecutorService service = Executors.newSingleThreadExecutor();
     public static final String NAME_OF_THREAD = "Statistics";
     public static final String SMILES_OR_EMOJI_MESSAGE = "What kind of statistics you want to see, names or emoji? Type the answer below";
     private static final String CARNIVORES_DEAD = "All carnivores are down. Please, stop tormenting this cursed world.";
     private static final String HERBIVORES_DEAD = "All herbivores died. We recommend that you stop torturing this weak world.";
-    private static String answer = "";
+    private static String answer = "emoji";
 
     /**
      * If you want to use this StatisticsPrinter in another world, just change "island" parameter
@@ -33,45 +40,52 @@ public class StatisticsPrinter implements Printer {
 
     @Override
     public void print() {
-        if (answer.isEmpty()) {
-            while (!answer.equalsIgnoreCase("emoji") || !answer.equalsIgnoreCase("names")) {
-                System.out.println(SMILES_OR_EMOJI_MESSAGE);
-                answer = new Scanner(System.in).nextLine();
-            }
-        }
-
         Thread.currentThread().setName(NAME_OF_THREAD);
         quantityOfPlants.clear();
         quantityOfAnimals.clear();
         carnivoresQuantity.set(0);
         herbivoresQuantity.set(0);
 
-        for (int x = 0; x < island.getWight(); x++) {
-            for (int y = 0; y < island.getHeight(); y++) {
-                Cell cell = island.getCell(x, y);
-                service.submit(() -> {
-                    if ("emoji".equalsIgnoreCase(answer)) {
-                        getStatisticsWithEmoji(cell);
-                    } else {
-                        getStatisticsWithNames(cell);
-                    }
-                });
-        }
-    }
+        Arrays.stream(island.getIslandField()).forEach(e -> Arrays.stream(e).forEach(cell -> service.submit(() -> {
+            if ("emoji".equalsIgnoreCase(answer)) {
+                getStatisticsWithEmoji(cell);
+            } else {
+                getStatisticsWithNames(cell);
+            }
+        })));
+
         try {
-            service.awaitTermination(ExecutorsAwaitingConstants.STATISTIC_PRINTER_AWAITING, TimeUnit.SECONDS);
+            if (service.awaitTermination(ExecutorsAwaitingConstants.STATISTIC_PRINTER_AWAITING, TimeUnit.SECONDS)) {
+                service.shutdownNow();
+            }
             service.shutdown();
         } catch (InterruptedException e) {
-            throw new RuntimeException(e + " " + "StatisticsPrinter is interrupted");
+            loggerToFile.error(e + " " + "StatisticsPrinter is interrupted");
+            Thread.currentThread().interrupt();
         }
-        quantityOfAnimals.forEach((k,v)->System.out.println(k +" : "+v ));
+        quantityOfAnimals.forEach((k,v)->loggerToConsole.log(Level.INFO, k +" : "+v ));
         System.out.println();
-        quantityOfPlants.forEach((k,v)->System.out.println(k +" : "+v ));
+        quantityOfPlants.forEach((k,v)->loggerToConsole.log(Level.INFO,k +" : "+v ));
         if (carnivoresQuantity.get() == 0) {
             System.out.println(CARNIVORES_DEAD);
 
         } else if (herbivoresQuantity.get() == 0) {
             System.out.println(HERBIVORES_DEAD);
+        }
+    }
+
+    private void decideNamesOrEmoji() {
+        if (answer.isEmpty()) {
+            while (!answer.equalsIgnoreCase("emoji") || !answer.equalsIgnoreCase("names")) {
+                loggerToConsole.log(Level.INFO ,SMILES_OR_EMOJI_MESSAGE);
+                choosingTypeOfStatisticsByUser();
+            }
+        }
+    }
+
+    private synchronized void choosingTypeOfStatisticsByUser() {
+        while (!answer.equals("emoji") || !answer.equals("names")) {
+            answer = new Scanner(System.in).nextLine();
         }
     }
 
